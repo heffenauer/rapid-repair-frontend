f96dcd3515e4fd11cd1ebc4e8bfc8062e43fc986
Din Becirbasic <dinbecirbasic@gmail.com>
2024-03-27 23:05:27 +0100

    added stuff

---
+++ src/app/booking-page/booking-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-page',
+  templateUrl: './booking-page.component.html',
+})
+export class BookingPageComponent {
+
+}
