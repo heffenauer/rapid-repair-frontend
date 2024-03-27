dace3a4d93a550592389cb8633e5c02925a216e0
Din Becirbasic <dinbecirbasic@gmail.com>
2024-03-27 23:05:27 +0100

    added stuff

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
+  { path: 'home', component: HomeComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
