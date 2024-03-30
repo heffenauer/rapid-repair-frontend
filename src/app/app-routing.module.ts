e861d0191df7eaaddc9825ac062bf7c4294b2c85
Adi Okerić <adi_okeric@hotmail.com>
2024-03-30 23:41:34 +0100

    Login Page update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
   { path: 'home', component: HomeComponent },
-  
+  { path: 'login', component: LoginPageComponent },
+
 ];
 
 @NgModule({
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
 
