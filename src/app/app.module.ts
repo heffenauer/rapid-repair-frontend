2d677e0dfa10238a3a1026f53e5bb7ee4d5d5037
Adi Okerić <adi_okeric@hotmail.com>
2024-03-30 17:25:38 +0100

    Added: Login Page componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
