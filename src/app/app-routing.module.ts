295af973365ecc9e643e4fc2ff58deb1af1d4b8b
Din Becirbasic <dinbecirbasic@gmail.com>
2024-04-01 14:10:51 +0200

    Added Register Page and Updated Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
   { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+
   
 ];
 
