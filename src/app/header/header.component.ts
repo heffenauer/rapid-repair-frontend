2d1054e0238fafb1311fbebd4822d984e24a4544
Adi Okerić <adi_okeric@hotmail.com>
2024-04-07 15:55:09 +0200

    Added service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter, map} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+currentRoute: string = '';
 
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          this.currentRoute = route.routeConfig?.path!;
+          console.log(this.currentRoute);
+          return route;
+        })
+      ).subscribe();
+  }
 }
---
+++ src/app/services/serviceman.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+
+@Injectable()
+export class ServicemanService{
+
+  constructor(private http:HttpClient) {
+  }
+
+
+  // @ts-ignore
+  public getSericeman(): Observable<ServicemanServiceDTO>{
+  // @ts-ignore
+    return this.http.get(ServicemanServiceDTO)(this.url)
+
+  }
+
+  public getAllServiceman(): Observable<any[]>{
+    return of([
+      {serviceManName:'Tom', age:28},
+      {serviceManName:'Tim', age:22}
+    ])
+  }
+
+
+
+
+}
