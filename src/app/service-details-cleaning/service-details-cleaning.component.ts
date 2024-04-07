bedb512d62c714c9acc53f7e3de6e22ce9d6329e
hadzerazukanovic <hadzera.zukanovic@stu.ssst.edu.ba>
2024-04-07 23:22:51 +0200

    Adds service details and booking pages

---
+++ src/app/service-details-cleaning/service-details-cleaning.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-service-details-cleaning',
+  templateUrl: './service-details-cleaning.component.html',
+
+})
+export class ServiceDetailsCleaningComponent {
+
+}
