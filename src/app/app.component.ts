c6e1f4243dd7ffe659bd9d8c76e96168299a3d2f
Din Becirbasic <dinbecirbasic@gmail.com>
2024-05-24 11:05:31 +0200

    Changed instances in files where project was named wrongly, for netlify hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'rapid-repair-frontend-temp';
+  title = 'rapid-repair-frontend';
 }
